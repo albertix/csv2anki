37a588a1ba05f90a146f77951ebfe01a94fab58f
albertix <albertix89@gmail.com>
2017-03-16 17:30:24 +0800

    json

--- csv2anki/Fcore.py
+++ csv2anki/Fcore.py
@@ -1,8 +1,49 @@
 
 class Collection(object):
 
-    def __init__(self, models, decks):
-        
+    def __init__(self, models):
+        '''
+        :param models:
+        '''
 
-    def make_decks(self):
+        self.tags = {}
+
+    @property
+    def conf(self):
         pass
+
+    @property
+    def dconf(self):
+        pass
+
+    @staticmethod
+    def from_jsons(models_json, decks_json):
+        models = [
+            (
+                [
+                    fld['name'] if not fld['rtl'] else fld['name'] + ':rtl'
+                    for fld in sorted(model['flds'], key=lambda fld: fld['ord'])],
+                [
+                    (tmpl['name'], tmpl['afmt'], tmpl['qfmt'])
+                    for tmpl in model['tmpls']],
+                model['css'])
+            for model
+            in (json.loads(models_json)).values()]
+
+        decks = 
+
+class Model(object):
+
+    CSS = ""
+
+    def __init__(self, flds, tmpls, css=None):
+        self.flds = flds
+        self.tmpls = tmpls
+        self.css = css if css else Models.CSS
+
+
+class NotesDeck(object):
+    def __init__(self, model, notes, deck_name):
+        self.model = model
+        self.notes = notes
+        self.deck_name = deck_name
