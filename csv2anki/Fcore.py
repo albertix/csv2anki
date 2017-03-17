64db91d0d270190bb0b01d189bc9837488340965
albertix <albertix89@gmail.com>
2017-03-17 17:17:47 +0800

    re design

--- csv2anki/Fcore.py
+++ csv2anki/Fcore.py
@@ -20,6 +20,7 @@ class Collection(object):
     def from_jsons(models_json, decks_json):
         models = [
             (
+                model['id'],
                 [
                     fld['name'] if not fld['rtl'] else fld['name'] + ':rtl'
                     for fld in sorted(model['flds'], key=lambda fld: fld['ord'])],
@@ -30,9 +31,36 @@ class Collection(object):
             for model
             in (json.loads(models_json)).values()]
 
-        decks =
+        decks = [
+            (
+                deck['id'],
+                deck['name'])
+            for deck in (json.loads(decks_json)).values()]
 
 class Model(object):
+    CSS = ''
+
+    def __init__(self, flds, tmpls, css=None, mid=None):
+        self.flds = flds
+        self.tmpls = tmpls
+        self.css = css if css else Model.CSS
+        self.id = mid
+
+    def gen_flds(self):
+        pass
+
+class Deck(object):
+    def __init__(self, models):
+        pass
+
+
+class Models(object):
+    '''
+    models = [
+        [id, flds, [[card_name1, [atmpl, qtmpl] ...], css]
+        ...
+    ]
+    '''
 
     CSS = ""
 
@@ -41,6 +69,34 @@ class Model(object):
         self.tmpls = tmpls
         self.css = css if css else Models.CSS
 
+    @staticmethod
+    def from_json(models_json):
+        models = [
+            {
+                'mid': model['id'],
+                'flds': [
+                    fld['name'] if not fld['rtl'] else fld['name'] + ':rtl'
+                    for fld in sorted(model['flds'], key=lambda fld: fld['ord'])],
+                'tmpls': [
+                    (tmpl['name'], tmpl['afmt'], tmpl['qfmt'])
+                    for tmpl in model['tmpls']],
+                'css': model['css']}
+            for model
+            in (json.loads(models_json)).values()]
+
+        models = [Model(**args) for args in models]
+        return models
+
+    @staticmethod
+    def to_json(models):
+        pass
+
+    @staticmethod
+    def from_csv(csv, deck_name):
+
+
+
+        return flds, notes, type, deck_name
 
 class NotesDeck(object):
     def __init__(self, model, notes, deck_name):
